f56b5b8d0a0cecb74ccd13f8b52693f64db931b8
David Knott <david1k1nott@gmail.com>
2017-01-23 20:56:28 -0700

    add merchant find to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      get "/merchants/find?parameters", to: "find_merchants#show"
       resources :merchants, only: [:index, :show]
       resources :customers, only: [:index, :show]
     end
