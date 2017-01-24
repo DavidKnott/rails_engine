85958c19ef374aa0221feeb9dde226202abdf0dd
David Knott <david1k1nott@gmail.com>
2017-01-23 23:19:15 -0700

    add find merchants index to router

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       get "/merchants/find", to: "find_merchants#show"
+      get "/merchants/find_all", to: "find_merchants#index"
       resources :merchants, only: [:index, :show]
       resources :customers, only: [:index, :show]
       resources :transactions, only: [:index, :show]
