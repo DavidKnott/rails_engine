d8915d8a27c3d5149e1a9e62551cf93c1ec9d037
David Knott <david1k1nott@gmail.com>
2017-01-23 23:17:44 -0700

    create merchant find all request test

--- spec/requests/api/v1/finds/finds_merchants_spec.rb
+++ spec/requests/api/v1/finds/finds_merchants_spec.rb
@@ -17,4 +17,23 @@ describe "Merchant Find Api" do
     expect(merchant).to have_key "created_at"
     expect(merchant).to have_key "updated_at"
   end
+
+  it "returns all merchants matching the given attribute" do
+    create_list(:merchant, 6, name: "John")
+    create_list(:merchant, 10, name: "Doe")
+
+    get "/api/v1/merchants/find_all?name=john"
+
+    merchants = JSON.parse(response.body)
+    merchant = merchants.first
+    expected_name = Merchant.find_by(name: "John").name
+
+    expect(response).to be_success
+    expect(merchants.count).to eq 6
+    expect(merchant).to have_key "id"
+    expect(merchant).to have_key "name"
+    expect(merchant["name"]).to eq expected_name
+    expect(merchant).to have_key "created_at"
+    expect(merchant).to have_key "updated_at"
+  end
 end
