ff12554b28b8ef20c2c2136f656c974554d0f3f3
David Knott <david1k1nott@gmail.com>
2017-01-24 11:32:38 -0700

    create transaction find all request test

--- spec/requests/api/v1/finds/finds_transactions_spec.rb
+++ spec/requests/api/v1/finds/finds_transactions_spec.rb
@@ -18,4 +18,23 @@ describe "Finds Transactions API" do
     expect(transaction).to have_key "created_at"
     expect(transaction).to have_key "updated_at"
   end
+
+  it "returns all transactions matching the given attributes" do
+    create(:transaction, credit_card_number: 12345, result: "failure")
+    create_list(:transaction, 10)
+
+    get "/api/v1/transactions/find_all?credit_card_number=12345&result=failure"
+
+    transaction = JSON.parse(response.body)
+    expected_card_number = Transaction.find_by(credit_card_number: 12345).credit_card_number
+
+    expect(response).to be_success
+    expect(transaction).to have_key "id"
+    expect(transaction).to have_key "credit_card_number"
+    expect(transaction["credit_card_number"]).to eq expected_cardnumber
+    expect(transaction).to have_key "result"
+    expect(transaction["result"]).to eq "failure"
+    expect(transaction).to have_key "created_at"
+    expect(transaction).to have_key "updated_at"
+  end
 end
