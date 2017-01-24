3761dc822fbb67a8eb5bbe7a21d6f282a929f7ab
David Knott <david1k1nott@gmail.com>
2017-01-23 19:44:06 -0700

    pass merchant index request test

--- spec/requests/api/v1/merchants_spec.rb
+++ spec/requests/api/v1/merchants_spec.rb
@@ -6,13 +6,16 @@ describe "Merchants API" do
 
     get "/api/v1/merchants"
 
-    expect(repsonse).to be_success
+    merchants = JSON.parse(response.body)
+    merchant = merchants.first
+
+    expect(response).to be_success
     expect(merchants.count).to eq 3
     expect(merchant).to be_a Hash
     expect(merchant).to have_key "id"
     expect(merchant).to have_key "name"
     expect(merchant["name"]).to be_a String
     expect(merchant).to have_key "created_at"
-    expect(merchant).to have_key "updated_ay"
+    expect(merchant).to have_key "updated_at"
   end
 end
