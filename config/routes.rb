e17f86efbcae4af62effd90669497f401a8a7ab5
Caroline Powell <carpowell10@gmail.com>
2017-01-23 21:54:24 -0700

    Add record index endpoint for invoices and invoice items

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
       resources :merchants, only: [:index]
       resources :customers, only: [:index]
       resources :items, only: [:index]
+      resources :invoices, only: [:index]
+      resources :invoice_items, only: [:index]
     end
   end
 end
---
+++ spec/requests/api/v1/invoice_items_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+describe "Invoice Items API" do
+  it "returns a list of invoice items" do
+    create_list(:invoice_item, 3)
+    get "/api/v1/invoice_items"
+    invoice_items_json = JSON.parse(response.body)
+    invoice_items = InvoiceItem.all
+
+    expect(response).to be_success
+    expect(invoice_items_json.count).to eql(3)
+    invoice_items_json.each do |invoice_item_json|
+      expect(invoice_item_json).to be_a(Hash)
+      expect(invoice_item_json).to have_key("id")
+      expect(invoice_item_json).to have_key("item_id")
+      expect(invoice_item_json).to have_key("invoice_id")
+      expect(invoice_item_json).to have_key("quantity")
+      expect(invoice_item_json).to have_key("unit_price")
+      expect(invoice_item_json).to have_key("created_at")
+      expect(invoice_item_json).to have_key("updated_at")
+    end
+    invoice_items.each_with_index do |invoice_item, i|
+      expect(invoice_items_json[i]["id"]).to eql(invoice_item.id)
+      expect(invoice_items_json[i]["item_id"]).to eql(invoice_item.item_id)
+      expect(invoice_items_json[i]["invoice_id"]).to eql(invoice_item.invoice_id)
+      expect(invoice_items_json[i]["quantity"]).to eql(invoice_item.quantity)
+      expect(invoice_items_json[i]["unit_price"]).to eql(invoice_item.unit_price)
+    end
+  end
+end
