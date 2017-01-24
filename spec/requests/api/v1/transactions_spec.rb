7a9f0ff97d5a531a2ce5f2a7935da9ef9616bda2
David Knott <david1k1nott@gmail.com>
2017-01-23 22:54:47 -0700

    pass transaction show request test

--- spec/requests/api/v1/transactions_spec.rb
+++ spec/requests/api/v1/transactions_spec.rb
@@ -6,12 +6,14 @@ describe "Transactions API" do
 
     get "/api/v1/transactions"
 
+    transactions = JSON.parse(response.body)
+    transaction = transactions.first
+
     expect(response).to be_success
     expect(transactions.count).to eq 3
     expect(transaction).to have_key "id"
     expect(transaction).to have_key "invoice_id"
     expect(transaction).to have_key "credit_card_number"
-    expect(transaction).to have_key "credit_card_expiration_date"
     expect(transaction).to have_key "result"
     expect(transaction["result"]).to be_a String
     expect(transaction).to have_key "created_at"
