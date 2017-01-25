1e37ba0b25161728f12657649b5cbcf1e3d82a2c
David Knott <david1k1nott@gmail.com>
2017-01-24 21:44:25 -0700

    create customer transactions relation request test

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -4,12 +4,19 @@ FactoryGirl.define do
     last_name "Dylan"
 
     transient do
-      invoice_count 3
+      invoices_count 3
+      transactions_count 3
     end
 
     factory :customer_with_invoices do
       after(:create) do |customer, evaluator|
-        create_list(:invoice, evaluator.invoice_count, customer: customer)
+        create_list(:invoice, evaluator.invoices_count, customer: customer)
+      end
+    end
+
+    factory :customer_with_invoices_with_transactions do
+      after(:create) do |customer, evaluator|
+        create_list(:invoice_with_transactions, evaluator.invoices_count, transactions_count: evaluator.transactions_count , customer: customer)
       end
     end
   end
--- spec/requests/api/v1/relations/customer_relations_spec.rb
+++ spec/requests/api/v1/relations/customer_relations_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "Customer Relations API" do
   it "returns all invoices for a given customer" do
-    customer = create_list(:customer_with_invoices, 3, invoice_count: 4).first
+    customer = create_list(:customer_with_invoices, 3, invoices_count: 4).first
 
     get "/api/v1/customers/#{customer.id}/invoices"
 
@@ -13,5 +13,20 @@ describe "Customer Relations API" do
     expect(invoices.count).to eq 4
     expect(invoice).to have_key "status"
     expect(invoice["customer_id"]).to eq customer.id
+  end
+
+  it "returns all transactions for a given customer" do
+    customer = create_list(:customer_with_invoices_with_transactions, 3, invoices_count: 2, transactions_count: 2).first
+
+    get "/api/v1/customers/#{customer.id}/transactions"
+
+    transactions = JSON.parse(response.body)
+    transaction = transactions.first
+
+    expect(response).to be_success
+    expect(transactions.count).to eq 4
+    expect(transaction).to have_key "credit_card_number"
+    byebug
+    expect(transaction).to eq customer.id
   end
 end
