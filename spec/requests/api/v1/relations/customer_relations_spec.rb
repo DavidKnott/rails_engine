d0acd2f66393bc4f30010dde1f51921e0660a260
David Knott <david1k1nott@gmail.com>
2017-01-24 21:20:41 -0700

    create customer invoice relation test

---
+++ spec/requests/api/v1/relations/customer_relations_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "Customer Relations API" do
+  it "returns all invoices for a given customer" do
+    customer = create_list(:customer_with_invoices, 3, invoice_count: 4).first
+
+    get "/api/v1/customers/#{customer.id}/invoices"
+
+    invoices = JSON.parse(response.body)
+    invoice = invoices.first
+
+    expect(response).to be_success
+    expect(invoices.count).to eq 4
+    expect(invoice).to have_key "status"
+    byebug
+    exepect(invoice).to eq cusomer.id
+  end
+end
