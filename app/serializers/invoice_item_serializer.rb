0961bb8187d0130afff13ef3090ff175d42eaa3a
David Knott <david1k1nott@gmail.com>
2017-01-25 16:38:25 -0700

    fix seed

--- app/serializers/invoice_item_serializer.rb
+++ app/serializers/invoice_item_serializer.rb
@@ -1,3 +1,12 @@
 class InvoiceItemSerializer < ActiveModel::Serializer
-  attributes :item_id, :invoice_id, :quantity, :unit_price
+  attributes :id, :item_id, :invoice_id, :quantity, :unit_price
+
+  def unit_price
+    cents_to_dollars(object.unit_price)
+  end
+
+  def cents_to_dollars(cents)
+    '%.2f' % (cents/100.0)
+  end
+
 end
