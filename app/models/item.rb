c64ab169138c6b8e6b3383000ead7791c06a58d0
Caroline Powell <carpowell10@gmail.com>
2017-01-26 14:43:50 -0700

    Add top items by revenue. Test failing due to order

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,13 @@ class Item < ApplicationRecord
   has_many :invoices, through: :invoice_items
 
   default_scope { order(:id)}
+
+  def self.most_revenue(top_x)
+    joins(:invoice_items)
+    	.joins(invoices: [:transactions])
+    	.where(transactions: {result: "success"})
+    .group("items.id")
+    .order("SUM(invoice_items.quantity * invoice_items.unit_price) DESC")
+    .limit(top_x)
+  end
 end
